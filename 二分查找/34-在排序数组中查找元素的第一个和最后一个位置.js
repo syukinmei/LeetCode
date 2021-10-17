8ead6e1b6448cd7fbed53847e9191f9e748d1199
syukinmei <347086009@qq.com>
2021-10-17 23:55:34 +0800

    10.17 Update:二分查找例题

---
+++ 二分查找/34-在排序数组中查找元素的第一个和最后一个位置.js
@@ -0,0 +1,64 @@
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+
+// 方法一：暴力解法
+// 利用数组有序的特点从头到尾遍历一次数组。
+// 在遍历的开始，检查遍历到的元素是否等于target，如果等于target，记录当前的下标位置
+// 接着遍历，检查遍历到的元素是否不等于target，如果不等于target，记录当前位置的前一个下标即可。
+// 复杂度分析：
+// 时间复杂度：O(n),这里n为数组的长度。
+// 空间复杂度：O(1)，只用到常数个临时变量。
+
+
+// 方法二：2次二分搜索分别检索大于等于target的部分 和 小于等于target的部分
+// 
+var searchRange = function (nums, target) {
+    let left = 0, right = nums.length - 1, mid, ans = [];
+    // 第一次二分搜索大于等于target的部分，即为target的起始位置
+    while (left < right) {
+        mid = left + ((right - left) >> 1);
+        if (nums[mid] >= target) { // 
+            right = mid;
+        } else {
+            left = mid + 1;
+        }
+    }
+    // 此时有left = right if该点为target即为第一个target的位置，else数组中无target，返回[-1,-1]
+    if (nums[left] === target) {
+        ans.push(left);
+    } else {
+        return [-1, -1];
+    }
+
+    // 第二次二分搜索小于等于target的部分
+    // 再次二分搜索初始化
+    left = 0, right = nums.length - 1;
+    // 会存在left 和 right 区间里都为target的情况如果mid = left会死循环；
+
+    // 此方法只能返回right
+    // while (left <= right) {
+    //     mid = left + ((right - left) >> 1);
+    //     if (nums[mid] <= target) {
+    //         left = mid + 1;
+    //     } else {
+    //         right = mid - 1;
+    //     }
+    // }
+
+    // 此方法可以left == right 都可以返回
+    while (left < right) {
+        mid = left + Math.ceil((right - left) / 2);
+        if (nums[mid] <= target) {
+            left = mid
+        } else {
+            right = mid - 1;
+        }
+    }
+    ans.push(right)
+    return ans;
+};
+searchRange([1, 2, 2, 4, 4, 4, 5, 6, 6, 6], 4)
