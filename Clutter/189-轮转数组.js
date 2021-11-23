29b70e6dc9447e40152ccd02a0751f950ea77fb7
syukinmei <347086009@qq.com>
2021-11-23 23:57:02 +0800

    11.23 Complement

--- Clutter/189-轮转数组.js
+++ Clutter/189-轮转数组.js
@@ -38,7 +38,31 @@ var rotate = function (nums, k) {
 
 
 // 方法二：数组反转
+// 当我们将数组的元素向右移动 k 次后，尾部 k mod n 个元素会移动至数组头部，其余元素向后移动 k mod n 个位置。
+// 我们可以先将所有元素翻转，这样尾部的 k mod n 个元素就被移至数组头部，然后我们再翻转 [0, k mod n -1] 区间的元素和 [k mod n, n-1] 区间的元素即能得到最后的答案。
 // nums = [1, 2, 3, 4, 5, 6, 7], k = 3
 // todo1 将整个数组反转 [7, 6, 5, 4, 3, 2, 1]
 // todo2 从第k个元素以后，将数组划分为左右两块子数组 [7, 6, 5] | [4, 3, 2, 1]
 // todo3 左右两子数组各自反转 [5, 6, 7] | [1, 2, 3, 4]
+
+// 辅助函数
+const reverse = (nums, start, end) => {
+    while (start < end) {
+        // 很多交换变量的方法在 344-反转字符串.js 中有介绍
+        let temp = nums[start];
+        nums[start] = nums[end];
+        nums[end] = temp;
+        start++;
+        end--;
+    }
+}
+
+var rotate = function (nums, k) {
+    // 解决k大于nums.length的情况
+    k %= nums.length;
+    reverse(nums, 0, nums.length - 1);
+    reverse(nums, 0, k - 1);
+    reverse(nums, k, nums.length - 1)
+}
+// 时间复杂度：o(n)，n为数组 nums 的长度，每个元素被翻转2次，一共 n 个元素，因此总时间复杂度为O(2n) = O(n)。
+// 空间复杂度：O(1)。
