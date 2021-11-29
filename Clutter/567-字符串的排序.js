4296ac9a07df10655c4215d706f399e126cc5101
syukinmei <347086009@qq.com>
2021-11-29 18:01:41 +0800

    11.29 Create

---
+++ Clutter/567-字符串的排序.js
@@ -0,0 +1,18 @@
+// 给你两个字符串 s1 和 s2 ，写一个函数来判断 s2 是否包含 s1 的排列。如果是，返回 true ；否则，返回 false 。
+// 换句话说，s1 的排列之一是 s2 的 子串 。
+
+// 输入：s1 = "ab" s2 = "eidbaooo"
+// 输出：true
+// 解释：s2 包含 s1 的排列之一 ("ba").
+
+// 输入：s1= "ab" s2 = "eidboaoo"
+// 输出：false
+
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @return {boolean}
+ */
+ var checkInclusion = function(s1, s2) {
+
+};
