9e8dd7b33643fac46d3eeab931345344462ca2ef
syukinmei <347086009@qq.com>
2021-12-04 21:12:06 +0800

    12.04 Complement

--- Clutter/1005-k次取反后最大化的数组和.js
+++ Clutter/1005-k次取反后最大化的数组和.js
@@ -33,22 +33,26 @@ var largestSumAfterKNegations = function (nums, k) {
             k = 0;
         }
     }
-    // 再次排序检查k的
-    nums.sort((a, b) => a - b);
-    for (let i = 0; i < nums.length && k % 2; i++) {
-        nums[i] *= -1;
-        k--;
+    // 如果还需反转，将数组排序第一个元素取反一次即可
+    if (k % 2) {
+        // 再次排序
+        nums.sort((a, b) => a - b);
+        nums[0] *= -1;
     }
     // 求和
     let sum = 0;
     for (let i = 0; i < nums.length; i++) {
         sum += nums[i];
     }
+    // let sum = nums.reduce((a, b) => a + b);
     return sum;
 };
 
 
-// 一次循环
+// 一次循环 + 贪心
+// 假设取反前的总和为 sum，取反一个任意值 x 后，对 sum 的影响为 - 2 * x。
+// 即取反一个负数会使得结果变大，取反正数会使结果变小，取反 0 值对结果没有影响
+
 var largestSumAfterKNegations = function (nums, k) {
     // 对数组依据绝对值生序排序
     nums.sort((a, b) => { return Math.abs(a) - Math.abs(b) });
@@ -65,7 +69,7 @@ var largestSumAfterKNegations = function (nums, k) {
     // 判断k剩余数量，为偶数直接return sum ，为奇数减2次最小数求反值
     if (k % 2) {
         // sum -= Math.abs(nums[0]) * 2
-        sum -= Math.abs(nums[0] << 1)
+        sum -= Math.abs(nums[0]) << 1;
     }
     return sum;
 };
