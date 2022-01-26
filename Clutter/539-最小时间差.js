1b6ba553ea5d428859ff61e5692df078d5471cbe
syukinmei <347086009@qq.com>
2022-01-26 15:33:12 +0800

    1.26 Correct

--- Clutter/539-最小时间差.js
+++ Clutter/539-最小时间差.js
@@ -13,16 +13,16 @@
  */
 var findMinDifference = function (timePoints) {
     let minutse = []; // 存储timePoints转分后的数组
-    for (let time of timePoints) {
-        let HM = time.split(':');
-        minutse.push(parseInt(HM[0] * 60) + parseInt(HM[1]));
+    for (let time of timePoints) { // 每一项转分后存入 minutse 中
+        let [HH, MM] = time.split(':');
+        minutse.push(parseInt(HH * 60) + parseInt(MM));
     }
     minutse.sort((a, b) => a - b); // 排序
-    // 遍历寻找，寻找最小时间差
-    let minGap = minutse[0] + 1440 - minutse[minutse.length - 1];
+    let ans = minutse[0] + 24 * 60 - minutse[minutse.length - 1]; // 首尾时间的时间差
     for (let i = 1; i < minutse.length; i++) {
-        let adjacentGap = minutse[i] - minutse[i - 1];
-        minGap = Math.min(minGap, adjacentGap);
+        ans = Math.min(ans, minutse[i] - minutse[i - 1]);
     }
-    return minGap;
+    return ans;
 };
+// 时间复杂度：O(nlogn)，n 为数组 timePoints 的长度。排序需要O(nlogn)的时间。
+// 空间复杂度：O(n)，n 为数组 timePoints 的长度，我们需要存储timePoints转分后的数组。
