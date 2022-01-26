50433922e00ded40447c948bf06dd7dbce1f882a
syukinmei <347086009@qq.com>
2022-01-26 17:11:00 +0800

    1.26 Complement

--- Clutter/1716-计算力扣银行的钱.js
+++ Clutter/1716-计算力扣银行的钱.js
@@ -28,6 +28,24 @@ var totalMoney = function (n) {
 // 时间复杂度：O(n)，需要遍历一次 n 得到答案。
 // 空间复杂度：O(1)，只需要常数的空间存放若干变量。
 
+
 // 方法二：等差数列求和公式
 // an = a1 + (n-1)d  Sn = n * a1 + n(n-1)d/2  或 Sn = n(a1+an)/2
 // 因为每周七天存的钱之和比上一周多 7 块，因此每周存的钱之和的序列是一个等差数列，我们可以用等差数列求和公式来求出所有完整的周存的钱总和。剩下的天数里，每天存的钱也是一个等差数列，可以用相同的公式进行求和。最后把两者相加可以得到答案。
+var totalMoney = function (n) {
+    // 1、利用等差公式计算 完整周存的钱
+    const weekNumber = Math.floor(n / 7); // 完整周的数量
+    const firstWeekMoney = 7 * 1 + 7 * (7 - 1) * 1 / 2; // 第一周存的钱
+    const lastWeekMoney = firstWeekMoney + (weekNumber - 1) * 7; // 最后一个完整周存的钱
+    const weekMoney = weekNumber * (firstWeekMoney + lastWeekMoney) / 2; // 完整周存的钱
+
+    // 2、利用等差公式计算 剩下不能构成一个完整周的天数里存的钱
+    dayNumber = n % 7; // 剩余不能构成一个完整周的天数
+    firstDayMoney = 1 + weekNumber; // 剩余天数第一天存的钱
+    lastDayMoney = firstDayMoney + dayNumber - 1; // 剩余天数最后一天存的钱
+    dayMoney = dayNumber * (firstDayMoney + lastDayMoney) / 2; // 剩下不能构成一个完整周的天数里存的钱
+    // dayMoney = dayNumber * firstDayMoney + dayNumber * (dayNumber - 1) * 1 / 2;
+    return dayMoney + weekMoney;
+}
+// 时间复杂度：O(1)，只需要用到常数时间。
+// 空间复杂度：O(1)，只需要常数的空间存放若干变量。
