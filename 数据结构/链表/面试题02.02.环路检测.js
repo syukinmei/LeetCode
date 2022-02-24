6f0ca2c7d1261d0fd57843091ed877e0beacee89
syukinmei <347086009@qq.com>
2022-02-24 17:01:59 +0800

    2.24 链表例题环路检测快慢指针实现

--- 数据结构/链表/面试题02.02.环路检测.js
+++ 数据结构/链表/面试题02.02.环路检测.js
@@ -21,6 +21,8 @@
 //    ^     |
 //    |_____|
 
+// PS：不存在环返回null，存在环返回环的起始节点。
+
 
 /**
  * Definition for singly-linked list.
@@ -53,3 +55,50 @@ var detectCycle = function (head) {
 // 空间复杂度：O(n)，n 为链表的节点数目。我们需要将链表中的每一个节点都保存到集合中。
 
 // 方法二：快慢指针
+// 步骤一：判断是否为有环链表
+//  - 定义两个指针 fast slow。fast 每次移动 2 位，slow 每次移动 1 位。
+//  - 如果相遇，则说明链表中存在环。
+
+// 步骤二：找到入环点
+//  - 如下图所示，设链表中环外部分的长度为 a。slow 指针进入环后，又走了 b 的距离与 fast 相遇。此时，fast 指针已经走完了环的 n 圈。
+//  - fast 移动距离为：a+n(b+c)+b 
+//  - slow 移动距离为：a+b
+//  - 又因： 速度比为 2:1 即在任意时刻 fast 指针走过的距离都为 slow 的 2倍
+//  - 得：a+n(b+c)+b = 2(a+b) 
+//  - 交互处理的：a = (n-1)(b+c)+c
+//  - 根据这层等量关系，我们可得：从相遇点到入环点的距离(图中c的长度)，恰好等于 从链表头部到入环点的距离。
+//  - 此时 再定一个一个新指针 ptr
+//  - 因此，当 slow 和 fast 相遇时，我们再额外定义一个 ptr 指针。起始阶段指向链表头部，然后 prt 和 slow 每次向后移动一个位置。最终会在入环点相遇。
+var detectCycle = function (head) {
+    // 特殊情况处理 
+    if (head === null || head.next === null) {
+        return null;
+    }
+    // 使用快慢指针判断是否存在环
+    let fast = head, slow = head;
+    while (fast !== null) {
+        slow = slow.next;
+        // fast 为尾部节点，则不存在环 返回null，否则移动两位
+        if (fast.next === null) {
+            return null;
+        }
+        fast = fast.next.next;
+        // 判断是否快慢指针是否相遇，相遇则存在环
+        if (slow === fast) {
+            // 找到入环点
+            let prt = head;
+            while (prt !== slow) {
+                prt = prt.next;
+                slow = slow.next;
+            }
+            // slow 和 prt 的交点即是入环点
+            return prt;
+        }
+    }
+    // while循环结束，说明 fast 为 null，即不存在环，返回null
+    return null;
+};
+// 时间复杂度：O(n)，n 为 链表的节点数目。
+//  - 第一步判断是否存在节点，slow 指针走过的距离不会超过链表的总长度；
+//  - 第二步寻找入环点，走过的距离也不会超过链表的总长度。
+// 空间复杂度：O(1)，只需要常数的空间存放若干变量，即 slow fast prt 三个指针。
