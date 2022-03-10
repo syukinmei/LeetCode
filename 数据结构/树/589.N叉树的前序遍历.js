9d638d49610c63b0dbf790802d1b661a0a97571c
syukinmei <347086009@qq.com>
2022-03-10 23:31:42 +0800

    3.10 DailyPractice

---
+++ 数据结构/树/589.N叉树的前序遍历.js
@@ -0,0 +1,37 @@
+// 给定一个 n 叉树的根节点  root ，返回 其节点值的 前序遍历 。
+// n 叉树 在输入中按层序遍历进行序列化表示，每组子节点由空值 null 分隔（请参见示例）。
+//        1
+//    /   |    \
+//   3    2     4
+//  / \
+// 5   6
+
+// 输入：root = [1,null,3,2,4,null,5,6]
+// 输出：[1,3,5,6,2,4]
+
+
+//           1
+//    /   |     |    \
+//   2    3     4     5
+//      / \     |     / \ 
+//     6   7    8    9   10
+//         |    |    |
+//         11   12   13
+//         |    
+//         14    
+
+// 输入：root = [1,null,2,3,4,5,null,null,6,7,null,8,null,9,10,null,null,11,null,12,null,13,null,null,14]
+// 输出：[1,2,3,6,7,11,14,4,8,12,5,9,13,10]
+
+/**
+ * // Definition for a Node.
+ * function Node(val, children) {
+ *    this.val = val;
+ *    this.children = children;
+ * };
+ */
+
+/**
+ * @param {Node|null} root
+ * @return {number[]}
+ */
