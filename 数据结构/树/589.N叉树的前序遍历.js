8c8334fc995e5af76770197800c02ac04fa982d8
syukinmei <347086009@qq.com>
2022-03-13 23:58:07 +0800

    3.13 N叉树的前序遍历(迭代)

--- 数据结构/树/589.N叉树的前序遍历.js
+++ 数据结构/树/589.N叉树的前序遍历.js
@@ -35,3 +35,35 @@
  * @param {Node|null} root
  * @return {number[]}
  */
+// 方法一：递归
+var preorder = function (root) {
+    if (root === null) [];
+    const res = [];
+    const helper = function (root) {
+        if (root === null) return; // 设置递归出口
+        res.push(root.val); // 记录遍历到的根节点值
+        for (let subtree of root.children) {
+            helper(subtree); // 前序遍历子树
+        }
+    }
+    helper(root);
+    return res;
+};
+// 时间复杂度：O(n)，n 为 N叉树的节点，每一个节点只被遍历一次。
+// 空间复杂度：O(n)，为递归过程中栈道开销，平均情况为O(logn)，最坏情况为树呈现链状，为O(n)。
+
+// 方法二：迭代
+var preorder = function (root) {
+    if (root === null) return [];
+    const res = [];
+    const stack = [];
+    stack.push(root);
+    while (stack.length !== 0) {
+        root = stack.pop(); // 栈底元素作为根节点 记录并遍历子树
+        res.push(root.val); // 记录节点
+        for (let i = root.children.length - 1; i >= 0; i--) {
+            stack.push(root.children[i]);
+        }
+    }
+    return res;
+};
