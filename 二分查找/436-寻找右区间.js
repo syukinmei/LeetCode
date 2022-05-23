0617fa8816bbe0607209d2daf4c20ae16968d6a3
syukinmei <347086009@qq.com>
2022-05-23 23:58:37 +0800

    5.23 AddInnovation 双指针

--- 二分查找/436-寻找右区间.js
+++ 二分查找/436-寻找右区间.js
@@ -75,3 +75,46 @@ var findRightInterval = function (intervals) {
 };
 // 时间复杂度：O(n logn)，n 为数组 intervals 的长度，排序的时间为 O(n logn)，每次进行二分查找花费的时间为O(logn)，一个需要进行 n 次二分，因此总的时间复杂度为O(nlogn)。
 // 空间复杂度：O(n)，n 为数组 intervals 的长度，startIntervals 一共存储了 n 个元素，因此空间复杂度为O(n)。
+
+// 方法二：双指针：
+// 和方法一一样，创建两个数组，startIntervals 和 endIntervals 
+// startIntervals 存储每个区间的起始位置 intervals[i][0] 以及对应的下标 i。
+// endIntervals   存储每个区间的结束位置 intervals[i][1] 以及对应的下标 i。
+// 对 startIntervals 和  endIntervals 根据区间大小进行升序排序
+// 如 intervals = [[2, 5], [1, 3], [3, 4]] ，此时就有：
+// startIntervals = [[1, 1], [2, 0], [3, 2]]    endIntervals = [[3, 1], [4, 2], [5, 0]]
+// 遍历 endIntervals[i][0]，从左到右扫描 startIntervals 数组中的起始区间，找到满足作为右区间条件的起始区间，记做 startIntervals[j][0]。
+// 此时 res[endIntervals[i][1]] = startIntervals[j][1];
+
+// 为什么使用双指针？
+// 已知 startIntervals[j-1][0] < endIntervals[i][0]， startIntervals[j][0] >= endIntervals[i][0]
+// 当我们遍历： endIntervals[i+1] 时，我们就不需要从第一个开始从左向右扫描 startIntervals 数组，可以直接从第 j 个元素开始扫描 startIntervals 数组。
+// 由于数组都是已经排序好的，因此可以知道 startIntervals[j-1][0] < endIntervals[i][0] <= endIntervals[i+1][0]，所以数组 startIntervals 的前 j-1 个区间的起始点一定都小于 endIntervals[i+1][0]，因此可以直接跳过 j-1 个元素。只需要从 j 开始扫描即可。
+var findRightInterval = function (intervals) {
+    const Len = intervals.length;
+    const startInterval = new Array(Len).fill(0).map(() => new Array(2).fill(0));
+    const endInterval = new Array(Len).fill(0).map(() => new Array(2).fill(0));
+    for (let i = 0; i < Len; i++) {
+        startInterval[i][0] = intervals[i][0];
+        startInterval[i][1] = i
+        endInterval[i][0] = intervals[i][1];
+        endInterval[i][1] = i;
+    }
+    startInterval.sort((a, b) => a[0] - b[0]);
+    endInterval.sort((a, b) => a[0] - b[0]);
+
+    const res = new Array(Len).fill(-1);
+    for (let i = 0, j = 0; i < Len; i++) {
+        const end = endInterval[i][0];
+        while (j < Len && startInterval[j][0] < end) {
+            j++;
+        }
+        // 此时找到了 end 的第一个右区间
+        if (j < Len) {
+            res[endInterval[i][1]] = startInterval[j][1];
+        }
+    }
+    return res;
+};
+// 时间复杂度：O(nlogn)，n 为区间数组 intervals 的长度，对两个数组的排序的时间一共需要 O(nlogn)，查找每个区间的右侧区间的时间复杂度为O(n),因此总的时间复杂度为O(nlogn)。
+// 空间复杂度：O(n)，n 为区间数组 intervals 的长度，startInterval 和 endInterval 均存储了 n 个元素，因此空间复杂度为O(n)。
