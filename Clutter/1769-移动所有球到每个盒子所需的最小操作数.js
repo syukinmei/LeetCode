fde472a17d601d90f8402122a2ffeae0db794f46
syukinmei <>
2022-12-05 00:15:32 +0800

    12.5 Complement:动态规划的解法

--- Clutter/1769-移动所有球到每个盒子所需的最小操作数.js
+++ Clutter/1769-移动所有球到每个盒子所需的最小操作数.js
@@ -37,3 +37,33 @@ var minOperations = function (boxes) {
 // 时间复杂度：O(n^2)，n 为数组 boxes 的长度，需要循环两次。
 // 空间复杂度：O(1)，只需要常数的空间存放若干变量。
 
+
+// 方法二：动态规划，根据前一个盒子的操作数得到下一个盒子的操作数
+// 针对盒子 i，其左侧有 lc 个小球，右侧有 rc 个小球。假设对盒子 i 的最少操作为 dp[i] 次。
+// 已知 dp[i]，可推导出 dp[i+1] 的最小操作数为 dp[i] + lc - rc。
+// 因为原来左侧的 lc 个小球各需要多操作1步，原来右侧的 rc 个小球可以各少操作1步。
+// 对于盒子 i 显然和 i-1 有递推关系。因此我们可以得到状态转移方程为：
+//  - dp[i] = dp[i-1] + 左边1个数 - 右边1个数。
+var minOperations = function (boxes) {
+    const res = new Array(boxes.length).fill(0);
+    let lc = boxes[0] === '1' ? 1 : 0; // 左侧 1 的总数
+    let rc = 0; // 右侧 1 的总数
+    for (let i = 1; i < boxes.length; i++) {
+        if (boxes[i] === '1') {
+            rc++; // 更新右侧 1 的总数
+            res[0] += i; // 第一个箱子的操作数
+        }
+    }
+
+    for (let i = 1; i < boxes.length; i++) {
+        res[i] = res[i - 1] + lc - rc; // 第 n 个箱子的操作数
+        if (boxes[i] === '1') {
+            // 更新维护 lc 和 rc。
+            lc++;
+            rc--;
+        }
+    }
+    return res;
+};
+// 时间复杂度：O(n)，n 为数组 boxes 的长度，我们分别需要对 boxes 数组循环两次。
+// 空间复杂度：O(1)，只需要常数的空间存放若干变量。
