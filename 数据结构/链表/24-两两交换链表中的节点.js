135816d64cf7e322c14826b2f2fd4dfd6a826e8c
syukinmei <347086009@qq.com>
2023-08-07 22:16:38 +0800

    8.07 Complement:添加解题思路和注解

--- 数据结构/链表/24-两两交换链表中的节点.js
+++ 数据结构/链表/24-两两交换链表中的节点.js
@@ -24,6 +24,23 @@
  * @return {ListNode}
  */
 // 方法一：迭代
+// 以 1 -> 2 -> 3 为例子
+
+// dummy -> 1 -> 2 -> 3
+
+//    -------1------
+//   |             |
+// dummy    1 <-3- 2    3
+//          |           |
+//          ------2------
+// 具体的，创建一个哨兵节点 dummy，令 dummy.next = head。令 cur 表示当前到达的节点，cur 之后的节点需要进行置换，初始时 cur = dummy。每次需要交换 cur 后面的两个节点。
+// 交换之前的节点关系是 cur -> node1 -> node2，交换之后的节点关系变成 cur -> node2 -> node1。因此需要进行如下操作：
+//  - cur.next = node2
+//  - node1.next = node2.next
+//  - node2.next = node1
+
+// 交换完成后更新 cur的位置，cur = node1，再对链表中其余节点进行两两交换，直到全部节点都被两两交换。
+// 两两交换链表中的节点后，新的链表的头节点就是 dummy.next。
 var swapPairs = function (head) {
   const dummy = new ListNode(-1); // 创建一个虚拟节点作为新链表的头节点的前一个节点
   dummy.next = head;
