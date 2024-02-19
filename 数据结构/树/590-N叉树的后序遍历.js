c393f8d9259797b6c56e4992a25aefc43f71a273
syukinmei <347086009@qq.com>
2024-02-19 23:31:03 +0800

    02.19 DailyPractice

--- 数据结构/树/590-N叉树的后序遍历.js
+++ 数据结构/树/590-N叉树的后序遍历.js
@@ -9,23 +9,22 @@
 // 输入：root = [1,null,3,2,4,null,5,6]
 // 输出：[5,6,3,2,4,1]
 
-
 //           1
 //    /   |     |    \
 //   2    3     4     5
-//      / \     |     / \ 
+//      / \     |     / \
 //     6   7    8    9   10
 //         |    |    |
 //         11   12   13
-//         |    
-//         14    
+//         |
+//         14
 
 // 输入：root = [1,null,2,3,4,5,null,null,6,7,null,8,null,9,10,null,null,11,null,12,null,13,null,null,14]
 // 输出：[2,6,14,11,7,3,12,8,4,13,9,10,5,1]
 
 /**
  * // Definition for a Node.
- * function Node(val, children) {
+ * function Node(val,children) {
  *    this.val = val;
  *    this.children = children;
  * };
@@ -36,6 +35,31 @@
  * @return {number[]}
  */
 // 方法一：递归
+// 每次递归时，先递归访问每个孩子节点，然后再访问根节点即可。
+// 递归函数的含义：
+// 对 N 叉树进行后序遍历，并将遍历得到的节点值添加到结果数组中。
+// 递归边界：
+// 当前节点为空。直接返回。
+// 递归内容：
+// 遍历所有子节点，并对每个子节点进行递归处理。
+// 递归返回值：无
 var postorder = function (root) {
+    // 用于存储遍历的结果
+    const res = [];
+    // 定义递归函数
+    const postorder = function (root) {
+        if (root === null) return; // 设置递归出口，当前节点为空时直接返回
+        // 后序遍历所有子节点
+        for (const childNode of root.children) {
+            postorder(childNode);
+        }
+        // 记录遍历到的根节点值
+        res.push(root.val);
+    };
 
+    // 调用递归函数从根节点开始后序遍历
+    postorder(root);
+    return res;
 };
+// 时间复杂度：O(n)，n 为 N叉树的节点数，每个节点只被访问一次。
+// 空间复杂度：O(n)，为递归过程中栈道开销，平均情况为O(logn)，最坏情况为树呈现链状，为O(n)。
