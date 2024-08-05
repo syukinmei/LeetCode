d177d874565984276f1f94c02c2665f3127e9f28
syukinmei <syukinmei321@163.com>
2024-08-05 15:33:59 +0800

    24.08.05 Complement:补充详细代码

--- Clutter/LCP 40-心算挑战.js
+++ Clutter/LCP 40-心算挑战.js
@@ -1,4 +1,5 @@
-// 「力扣挑战赛」心算项目的挑战比赛中，要求选手从 N 张卡牌中选出 cnt 张卡牌，若这 cnt 张卡牌数字总和为偶数，则选手成绩「有效」且得分为 cnt 张卡牌数字总和。 给定数组 cards 和 cnt，其中 cards[i] 表示第 i 张卡牌上的数字。 请帮参赛选手计算最大的有效得分。若不存在获取有效得分的卡牌方案，则返回 0。
+// 「力扣挑战赛」心算项目的挑战比赛中，要求选手从 N 张卡牌中选出 cnt 张卡牌，若这 cnt 张卡牌数字总和为偶数，则选手成绩「有效」且得分为 cnt 张卡牌数字总和。
+// 给定数组 cards 和 cnt，其中 cards[i] 表示第 i 张卡牌上的数字。 请帮参赛选手计算最大的有效得分。若不存在获取有效得分的卡牌方案，则返回 0。
 
 // 输入：cards = [1,2,8,9], cnt = 3
 // 输出：18
@@ -21,4 +22,39 @@
  * @param {number} cnt
  * @return {number}
  */
-var maxmiumScore = function (cards, cnt) {};
+var maxmiumScore = function (cards, cnt) {
+    let sum = 0;
+    // 用于记录最大的 cnt 张排中最小的奇数和偶数
+    let minOdd = -1;
+    let minEven = -1;
+
+    cards.sort((a, b) => b - a); // 将卡牌根据面值降序排序
+
+    // 计算最大的 cnt 张牌的和
+    for (let i = 0; i < cnt; i++) {
+        sum += cards[i];
+
+        if (cards[i] & 1) minOdd = cards[i];
+        else minEven = cards[i];
+    }
+
+    // 如果和为偶数，直接返回
+    if ((sum & 1) === 0) return sum;
+
+    // 如果和为奇数，尝试去掉一个最小的奇数，然后加进来一个最大的偶数，或者去掉一个最小的偶数，然后加进来一个最大的奇数
+    let ans = 0;
+    for (let i = cnt; i < cards.length; i++) {
+        // 奇数 - 当前遇到了教大的奇数，去掉前 cnt 个数中的最小偶数，然后将这个奇数加入其中
+        if ((cards[i] & 1) === 1 && minEven !== -1) {
+            ans = Math.max(ans, sum - minEven + cards[i]);
+        }
+        // 偶数 - 当前遇到了教大的偶数，去掉前 cnt 个数中的最小奇数，然后将这个偶数加入其中
+        if ((cards[i] & 1) === 0 && minOdd !== -1) {
+            ans = Math.max(ans, sum - minOdd + cards[i]);
+        }
+    }
+
+    return ans;
+};
+// 时间复杂度：O(nlogn)，n 为数组 cards 的长度。排序需要 O(nlogn)的时间，构建最大的有效得分需要对 cards 进行一次遍历，时间复杂度为O(n)，因此总的时间复杂度为 O(nlogn)。
+// 空间复杂度：O(logn)，快排需要 O(logn)的递归栈空间。
