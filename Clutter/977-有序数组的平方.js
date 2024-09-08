2450da7ec8c3c531221678fd4729b1dbeaa61482
syukinmei <347086009@qq.com>
2024-09-08 23:39:25 +0800

    24.09.08 Complement:补充解题思路

--- Clutter/977-有序数组的平方.js
+++ Clutter/977-有序数组的平方.js
@@ -9,24 +9,35 @@
 // 输出：[4,9,9,49,121]
 
 // 方法一：双指针
-// 数组是有序的， 只不过负数平方之后可能成为最大数了。那么数组平方的最大值就在数组的两端，不是最左边就是最右边，不可能是中间。
-// 定义一个新数组res，和nums数组一样的大小，让k指向res数组终止位置。
-// 判断nums数组两端平方后谁为最大值，给数组res倒着赋值。
-// 如果nums[i] * nums[i] < nums[j] * nums[j] 那么result[k--] = nums[j] * nums[j]; 
-// 如果nums[i] * nums[i] >= nums[j] * nums[j] 那么result[k--] = nums[i] * nums[i]; 
+// 数组是有序的，只不过负数平方之后可能成为最大数了。那么数组平方的最大值就在数组的两端，不是最左边就是最右边，不可能是中间。
+// 因此，我们可以使用「双指针」，分别指向数组的两端，每次比较两个指针指向的元素的平方值，将较大者的平方值放入结果数组的末尾。
+// 具体的：
+// 定义一个新数组 res，和 nums 数组一样的大小，让 k 指向 res 数组的末尾位置；
+// 初始化左右指针指向数组两端；
+// 比较左右指针指向的元素的平方值，将较大的值放入 res[k]，并将对应指针向中间移动一位：
+//  - 判断 nums 数组两端平方后谁为最大值，给数组res倒着赋值；
+//  - 如果nums[i] * nums[i] < nums[j] * nums[j] 那么res[k--] = nums[j] * nums[j];
+//  - 如果nums[i] * nums[i] >= nums[j] * nums[j] 那么res[k--] = nums[i] * nums[i];
+// 循环直到所有数据已填入 res 中（直到左指针超过右指针，或者 k 指针为 -1）；
+// 返回 res ；
 
 /**
  * @param {number[]} nums
  * @return {number[]}
  */
 var sortedSquares = function (nums) {
-    let left = 0, right = nums.length - 1;
-    const res = new Array(nums.length);
-    let k = res.length - 1;
+    // 初始化左右指针、插入指针 k，结果数组 res
+    let left = 0,
+        right = nums.length - 1;
+    const n = nums.length;
+    let k = n - 1;
+    const res = new Array(n);
+
+    // 循环，直到左指针超过右指针，或者 k 指针为 -1，均表示所有数据已填入 res 中
     while (left <= right) {
         // 相等的情况谁赋值都一样
         if (nums[left] * nums[left] >= nums[right] * nums[right]) {
-            res[k--] = nums[left] * nums[left]
+            res[k--] = nums[left] * nums[left];
             left++;
         } else if (nums[left] * nums[left] < nums[right] * nums[right]) {
             res[k--] = nums[right] * nums[right];
